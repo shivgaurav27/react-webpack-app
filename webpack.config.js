0730596271eed4b26aba5742a7a8865ddc041abf
shiv7727 <shiv7727@gmail.com>
2025-07-10 12:40:27 +0530

    webpack optimize bundle added

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = (env, argv) => {
 	const isDev = argv.mode === 'development';
 	return {
 		entry: './src/index.js',
 		output: {
-			filename: 'bundle.js',
+			filename: isDev ? 'bundle.js' : 'bundle.[contenthash].js',
 			path: path.resolve(__dirname, 'dist'),
 			clean: true,
 		},
@@ -37,10 +40,23 @@ module.exports = (env, argv) => {
 		resolve: {
 			extensions: ['.js', '.jsx'],
 		},
+		optimization: {
+			minimize: !isDev,
+			minimizer: ['...', new CssMinimizerPlugin()],
+			splitChunks: {
+				chunks: 'all',
+			},
+		},
 		plugins: [
 			new HtmlWebPackPlugin({
 				template: './public/index.html',
 			}),
-		],
+			new BundleAnalyzerPlugin(),
+			!isDev &&
+				new CompressionPlugin({
+					algorithm: 'gzip',
+					test: /\.(js|css|html|svg)$/,
+				}),
+		].filter(Boolean),
 	};
 };
